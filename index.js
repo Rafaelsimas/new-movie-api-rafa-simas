ba177bd0e327b291a11ec5be2b86d824e05aaee3
rafael <rafael.simas@al.resilia.com.br>
2022-08-31 13:07:43 -0300

    Use cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const filmes = require('./src/data/filmes.json')
 
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/filmes', (req, res) =>{
     return res.json(filmes)
